744c619e180696c84346093f8c268dbf0fcc3cc7
Namtoan2001 <toancule2001@gmail.com>
2021-10-11 16:22:26 +0700

    first

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Ung-dung-nghe-nhac-Rabidly" 
+"# Ung-dung-nghe-nhac-Rabidly" 
---
+++ app/src/main/java/com/example/ung_dung_nghe_nhac_rabidly/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ung_dung_nghe_nhac_rabidly;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
